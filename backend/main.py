7eecf5b32eb7fbe11105eb27ab0a23056994d4d3
Abhijeet Jha <jhaabhijeet864@gmail.com>
2026-01-31 18:22:43 +0530

    Port change.

--- backend/main.py
+++ backend/main.py
@@ -33,8 +33,8 @@ app.add_middleware(
         "http://127.0.0.1:5501",
         "http://localhost:5500",
         "http://localhost:5501",
-        "http://127.0.0.1:8000",
-        "http://localhost:8000"
+        "http://127.0.0.1:8080",
+        "http://localhost:8080"
     ],
     allow_credentials=True,
     allow_methods=["*"],
